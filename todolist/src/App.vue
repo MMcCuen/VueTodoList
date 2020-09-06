f1e8b7320978640f8da8b8e0b52cf7898617f763
Mitch McCuen <mitchmccuen@gmail.com>
2020-09-06 13:38:40 -0400

    todo items displayed

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -1,11 +1,36 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Todos v-bind:todos="todos" />
+  </div>
 </template>
 
 <script>
+import Todos from "./components/Todos";
+
 export default {
   name: "App",
-  components: {},
+  components: { Todos },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false,
+        },
+        {
+          id: 2,
+          title: "Todo two",
+          completed: true,
+        },
+        {
+          id: 3,
+          title: "Todo 3",
+          completed: false,
+        },
+      ],
+    };
+  },
 };
 </script>
 
@@ -20,3 +45,4 @@ body {
   line-height: 1.4;
 }
 </style>
+
---
+++ todolist/src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="todo-item">
+    <p>{{todo.title}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+};
+</script>
+
+<style>
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  border-bottom: 1px #ccc dotted;
+}
+.is-complete {
+  text-decoration: line-through;
+}
+.del {
+  background: #ff0000;
+}
+</style>
